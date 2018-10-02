081d5e2857511a5aba0e97773aa2175cf07e179d
garricknorthover <garricknorthover@gmail.com>
2018-10-03 10:30:03 +1300

    learning styled components and alt folder structure with components folder

---
+++ src/components/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Line = styled.div`
+background: blue;
+${props => props.other && css`
+background: red;
+`}
+`
+export default function App () {
+  return (
+    <div>
+      <h1>Tic-Tac-Toe</h1>
+      <Line>hgjhg</Line>
+      <Line other>hgjhg</Line>
+
+    </div>
+  )
+}
