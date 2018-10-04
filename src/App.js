54b1448ea1c5568376a69bec897fb545307fd5a4
garricknorthover <garricknorthover@gmail.com>
2018-10-04 14:16:31 +1300

    leaning object to component stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
 
-const Line = styled.div`
-background: blue;
-${props => props.other && css`
-background: red;
-`}
-`
+const items = [
+  {
+    id: 1,
+    title: 'the star',
+    content: 'is twunkling in the sky'
+  },
+  {
+    id: 2,
+    title: 'the bear',
+    content: 'is shitting in the woods'
+  },
+  {
+    id: 3,
+    title: 'the moon',
+    content: 'is a balloon'
+  }
+]
+
+const ListItems = ({ ListObject }) =>
+
+  ListObject.map(item => (
+
+    <tr key={item.id}>
+      <td>{item.title}</td>
+      <td>{item.content}</td>
+    </tr>
+
+  ))
+
 export default () => (
   <div>
-    <h1>Email program</h1>
-    <Line>hgjhg</Line>
-    <Line other>hgjhg</Line>
+    <h1>List of something</h1>
+    <ListItems ListObject={items} />
   </div>
 )
-
