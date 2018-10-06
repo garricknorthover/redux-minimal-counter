d3be222a4ff152055edcfe789684e3ec66cb59e8
garricknorthover <garricknorthover@gmail.com>
2018-10-06 14:45:22 +1300

    some basic stylin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { increment, decrement } from './store/actions'
+import styled from 'styled-components'
+
+const Button = styled.button`
+background-color: papayawhip;
+
+`
+const Circle = styled.div`
+  border-radius: 100px;
+  background-color: papayawhip;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const App = ({ counter, increment, decrement }) => (
   <div>
-    <div>{counter}</div>
-    <button onClick={increment}>+</button>
-    <button onClick={decrement}>-</button>
+    <Circle>{counter}</Circle>
+    <Button onClick={increment}>+</Button>
+    <Button onClick={decrement}>-</Button>
   </div>
 )
 
